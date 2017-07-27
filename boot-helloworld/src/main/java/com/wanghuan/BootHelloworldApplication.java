d3b7afca6247307d6b854e7fc02034c04826fecb
zbhuanhuan <544168502@qq.com>
2017-07-27 11:17:58 +0800

    建立helloworld项目

---
+++ boot-helloworld/src/main/java/com/wanghuan/BootHelloworldApplication.java
@@ -0,0 +1,12 @@
+package com.wanghuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootHelloworldApplication.class, args);
+	}
+}
