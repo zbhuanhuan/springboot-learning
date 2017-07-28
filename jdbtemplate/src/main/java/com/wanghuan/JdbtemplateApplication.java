358de1c05c60b7aff67ed5e9e78297663aa39c6c
zbhuanhuan <544168502@qq.com>
2017-07-28 11:55:41 +0800

    springboot结合jdbc的使用

---
+++ jdbtemplate/src/main/java/com/wanghuan/JdbtemplateApplication.java
@@ -0,0 +1,12 @@
+package com.wanghuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbtemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbtemplateApplication.class, args);
+	}
+}
